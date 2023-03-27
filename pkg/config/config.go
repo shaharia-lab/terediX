ec4595e7795bd36d474595043e522edacc007af1
Shaharia Azam <mail@shaharia.com>
2023-03-27 22:49:54 +0200

    Re-factor config.go

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -74,35 +74,72 @@ func Load(path string) (*AppConfig, error) {
 
 // Validate Add this method to your AppConfig struct
 func Validate(c *AppConfig) error {
-	if c.Organization.Name == "" {
+	err := c.validateOrganization(c.Organization)
+	if err != nil {
+		return err
+	}
+
+	err = c.validateDiscovery(c.Discovery)
+	if err != nil {
+		return err
+	}
+
+	err = c.validateStorage(c.Storage, c.Sources)
+	if err != nil {
+		return err
+	}
+
+	err = c.validateSources(c.Sources)
+	if err != nil {
+		return err
+	}
+
+	err = c.validateRelations(c.Relation)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *AppConfig) validateOrganization(org Organization) error {
+	if org.Name == "" {
 		return fmt.Errorf("organization name is required")
 	}
 
-	if c.Discovery.Name == "" {
+	return nil
+}
+
+func (c *AppConfig) validateDiscovery(discovery Discovery) error {
+	if discovery.Name == "" {
 		return fmt.Errorf("discovery name is required")
 	}
 
-	if c.Storage.BatchSize <= 0 {
+	return nil
+}
+
+func (c *AppConfig) validateStorage(storage Storage, sources map[string]Source) error {
+	if storage.BatchSize <= 0 {
 		return fmt.Errorf("storage batch_size must be greater than 0")
 	}
 
-	if len(c.Storage.Engines) == 0 {
+	if len(storage.Engines) == 0 {
 		return fmt.Errorf("at least one storage engine must be defined")
 	}
 
-	if _, ok := c.Storage.Engines[c.Storage.DefaultEngine]; !ok {
+	if _, ok := storage.Engines[storage.DefaultEngine]; !ok {
 		return fmt.Errorf("default storage engine must be one of the defined engines")
 	}
 
 	// Validate storage engines
-	for engine, config := range c.Storage.Engines {
+	for engine, config := range storage.Engines {
 		switch engine {
 		case "postgresql":
 			if err := c.validatePostgresqlEngine(config); err != nil {
 				return err
 			}
 		case "neo4j":
-			if err := c.validateNeo4jEngine(config); err != nil {
+			if err := validateNeo4jEngine(config); err != nil {
 				return err
 			}
 		default:
@@ -110,51 +147,130 @@ func Validate(c *AppConfig) error {
 		}
 	}
 
-	if len(c.Sources) == 0 {
+	if len(sources) == 0 {
 		return fmt.Errorf("at least one source must be defined")
 	}
 
-	for name, source := range c.Sources {
+	return nil
+}
+
+func (c *AppConfig) validatePostgresqlEngine(config interface{}) error {
+	configMap, ok := config.(map[string]interface{})
+	if !ok {
+		return fmt.Errorf("postgresql engine configuration must be a map")
+	}
+
+	requiredKeys := []string{"host", "user", "password", "db", "port"}
+	for _, key := range requiredKeys {
+		if _, ok := configMap[key]; !ok {
+			return fmt.Errorf("postgresql engine requires '%s'", key)
+		}
+	}
+	return nil
+}
+
+func validateNeo4jEngine(config interface{}) error {
+	configMap, ok := config.(map[string]interface{})
+	if !ok {
+		return fmt.Errorf("neo4j engine configuration must be a map")
+	}
+
+	if _, ok := configMap["config_key"]; !ok {
+		return fmt.Errorf("neo4j engine requires 'config_key'")
+	}
+	return nil
+}
+
+func (c *AppConfig) validateSources(sources map[string]Source) error {
+	if len(sources) == 0 {
+		return fmt.Errorf("at least one source must be defined")
+	}
+
+	for name, source := range sources {
 		if source.Type == "" {
 			return fmt.Errorf("source '%s' type is required", name)
 		}
 
-		const errorTextFormat = "source '%s': %v"
-
-		switch source.Type {
-		case pkg.SourceTypeFileSystem:
-			if err := c.validateFileSystemSource(source); err != nil {
-				return fmt.Errorf(errorTextFormat, name, err)
-			}
-		case "kubernetes":
-			if err := c.validateKubernetesSource(source); err != nil {
-				return fmt.Errorf(errorTextFormat, name, err)
-			}
-		case pkg.SourceTypeGitHubRepository:
-			if err := c.validateGitHubRepositorySource(source); err != nil {
-				return fmt.Errorf(errorTextFormat, name, err)
-			}
-		default:
-			return fmt.Errorf("unknown source type: '%s'", source.Type)
+		if err := c.validateSourceConfiguration(source.Type, source); err != nil {
+			return fmt.Errorf("source '%s': %v", name, err)
 		}
 
-		// Validate depends_on field
-		for _, dependency := range source.DependsOn {
-			if _, ok := c.Sources[dependency]; !ok {
-				return fmt.Errorf("source '%s' depends_on contains invalid source key: '%s'", name, dependency)
-			}
+		if err := c.validateDependsOn(name, source); err != nil {
+			return fmt.Errorf("source '%s': %v", name, err)
 		}
 	}
 
-	if c.Relation.RelationCriteria == nil {
+	return nil
+}
+
+func (c *AppConfig) validateSourceConfiguration(name string, source Source) error {
+	switch source.Type {
+	case pkg.SourceTypeFileSystem:
+		if err := c.validateFileSystemSourceConfiguration(name, source); err != nil {
+			return err
+		}
+	case "kubernetes":
+		if err := c.validateKubernetesSourceConfiguration(name, source); err != nil {
+			return err
+		}
+	case pkg.SourceTypeGitHubRepository:
+		if err := c.validateGitHubRepositorySourceConfiguration(name, source); err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("unknown source type: '%s'", source.Type)
+	}
+	return nil
+}
+
+func (c *AppConfig) validateFileSystemSourceConfiguration(name string, source Source) error {
+	rootDirectory, ok := source.Configuration["root_directory"]
+	if !ok || rootDirectory == "" {
+		return fmt.Errorf("source '%s' requires 'configuration.root_directory'", name)
+	}
+	return nil
+}
+
+func (c *AppConfig) validateKubernetesSourceConfiguration(name string, source Source) error {
+	kubeConfigFilePath, ok := source.Configuration["kube_config_file_path"]
+	if !ok || kubeConfigFilePath == "" {
+		return fmt.Errorf("source '%s' requires 'configuration.kube_config_file_path'", name)
+	}
+	return nil
+}
+
+func (c *AppConfig) validateGitHubRepositorySourceConfiguration(name string, source Source) error {
+	ghToken, ok := source.Configuration["token"]
+	if !ok || ghToken == "" {
+		return fmt.Errorf("source '%s' requires 'configuration.token'", name)
+	}
+
+	userOrOrg, ok := source.Configuration["user_or_org"]
+	if !ok || userOrOrg == "" {
+		return fmt.Errorf("source '%s' requires 'configuration.user_or_org'", name)
+	}
+	return nil
+}
+
+func (c *AppConfig) validateDependsOn(name string, source Source) error {
+	for _, dependency := range source.DependsOn {
+		if _, ok := c.Sources[dependency]; !ok {
+			return fmt.Errorf("source '%s' depends_on contains invalid source key: '%s'", name, dependency)
+		}
+	}
+	return nil
+}
+
+func (c *AppConfig) validateRelations(relations Relation) error {
+	if relations.RelationCriteria == nil {
 		return fmt.Errorf("relations field must be defined")
 	}
 
-	if len(c.Relation.RelationCriteria) == 0 {
+	if len(relations.RelationCriteria) == 0 {
 		return fmt.Errorf("relations.criteria is empty")
 	}
 
-	for _, criteria := range c.Relation.RelationCriteria {
+	for _, criteria := range relations.RelationCriteria {
 		if criteria.Name == "" {
 			return fmt.Errorf("relations.criteria.name is required")
 		}
@@ -184,61 +300,5 @@ func Validate(c *AppConfig) error {
 		}
 	}
 
-	return nil
-}
-
-func (c *AppConfig) validatePostgresqlEngine(config interface{}) error {
-	configMap, ok := config.(map[string]interface{})
-	if !ok {
-		return fmt.Errorf("postgresql engine configuration must be a map")
-	}
-
-	requiredKeys := []string{"host", "user", "password", "db", "port"}
-	for _, key := range requiredKeys {
-		if _, ok := configMap[key]; !ok {
-			return fmt.Errorf("postgresql engine requires '%s'", key)
-		}
-	}
-	return nil
-}
-
-func (c *AppConfig) validateNeo4jEngine(config interface{}) error {
-	configMap, ok := config.(map[string]interface{})
-	if !ok {
-		return fmt.Errorf("neo4j engine configuration must be a map")
-	}
-
-	if _, ok := configMap["config_key"]; !ok {
-		return fmt.Errorf("neo4j engine requires 'config_key'")
-	}
-	return nil
-}
-
-func (c *AppConfig) validateFileSystemSource(source Source) error {
-	rootDirectory, ok := source.Configuration["root_directory"]
-	if !ok || rootDirectory == "" {
-		return fmt.Errorf("%s source requires 'configuration.root_directory'", pkg.SourceTypeFileSystem)
-	}
-	return nil
-}
-
-func (c *AppConfig) validateKubernetesSource(source Source) error {
-	kubeConfigFilePath, ok := source.Configuration["kube_config_file_path"]
-	if !ok || kubeConfigFilePath == "" {
-		return fmt.Errorf("kubernetes source requires 'configuration.kube_config_file_path'")
-	}
-	return nil
-}
-
-func (c *AppConfig) validateGitHubRepositorySource(source Source) error {
-	ghToken, ok := source.Configuration["token"]
-	if !ok || ghToken == "" {
-		return fmt.Errorf("%s source requires 'configuration.token'", pkg.SourceTypeGitHubRepository)
-	}
-
-	userOrOrg, ok := source.Configuration["user_or_org"]
-	if !ok || userOrOrg == "" {
-		return fmt.Errorf("%s source requires 'configuration.user_or_org'", pkg.SourceTypeGitHubRepository)
-	}
 	return nil
 }
