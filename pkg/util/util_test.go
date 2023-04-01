6b18f59a5a888a5843a1d7ddb896339c31b40fd0
Shaharia Azam <mail@shaharia.com>
2023-04-02 01:39:52 +0200

    Test improved

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -3,9 +3,9 @@ package util
 import (
 	"errors"
 
-	"testing"
-
 	"github.com/stretchr/testify/assert"
+
+	"testing"
 )
 
 func TestGenerateUUID(t *testing.T) {
@@ -19,22 +19,46 @@ func TestGenerateUUID(t *testing.T) {
 }
 
 func TestRetryWithExponentialBackoff(t *testing.T) {
-	// Define the function to retry
-	fn := func() error {
-		return errors.New("error")
+	testCases := []struct {
+		name        string
+		callback    func() error
+		expectError bool
+	}{
+		{
+			name: "function returns error",
+			callback: func() error {
+				return errors.New("error")
+			},
+			expectError: true,
+		},
+		{
+			name: "function returns no error",
+			callback: func() error {
+				return nil
+			},
+			expectError: false,
+		},
 	}
 
-	// Define the maximum number of retries and the backoff intervals
-	maxRetries := 1
-	initialBackoff := 1
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Define the maximum number of retries and the backoff intervals
+			maxRetries := 1
+			initialBackoff := 1
 
-	// Call the function with retry and check the result
-	err := RetryWithExponentialBackoff(fn, maxRetries, initialBackoff)
-	assert.Error(t, err)
+			// Call the function with retry and check the result
+			err := RetryWithExponentialBackoff(tc.callback, maxRetries, initialBackoff)
 
-	// Check the error message
-	expectedErrorMessage := "maximum number of retries exceeded: error"
-	if err.Error() != expectedErrorMessage {
-		t.Errorf("unexpected error message: got %v, want %v", err.Error(), expectedErrorMessage)
+			if tc.expectError {
+				assert.Error(t, err)
+				// Check the error message
+				expectedErrorMessage := "maximum number of retries exceeded: error"
+				if err.Error() != expectedErrorMessage {
+					t.Errorf("unexpected error message: got %v, want %v", err.Error(), expectedErrorMessage)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+		})
 	}
 }
