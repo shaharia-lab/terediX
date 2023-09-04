20383f1155206ed05627e6f43d8182a71fb4b774
Shaharia Azam <mail@shaharia.com>
2023-09-04 20:27:01 +0200

    Meta data re-factoring

--- pkg/resource/resource.go
+++ pkg/resource/resource.go
@@ -27,6 +27,7 @@ func (ml *MetaDataLists) IsExists(key string) bool {
 	return false
 }
 
+// Add adds metadata
 func (ml *MetaDataLists) Add(key, value string) {
 	if ml.IsExists(key) {
 		return
@@ -35,12 +36,14 @@ func (ml *MetaDataLists) Add(key, value string) {
 	ml.MetaData = append(ml.MetaData, MetaData{Key: value})
 }
 
+// AddMap adds metadata from map
 func (ml *MetaDataLists) AddMap(metaMap map[string]string) {
 	for k, v := range metaMap {
 		ml.Add(k, v)
 	}
 }
 
+// Find returns metadata by key
 func (ml *MetaDataLists) Find(key string) *MetaData {
 	if !ml.IsExists(key) {
 		return nil
@@ -81,42 +84,52 @@ func NewResource(kind, name, externalID, scannerName, version string) Resource {
 	}
 }
 
+// SetUUID sets resource UUID
 func (r *Resource) SetUUID(uuid string) {
 	r.uuid = uuid
 }
 
+// GetScanner returns resource scanner
 func (r *Resource) GetScanner() string {
 	return r.scanner
 }
 
+// GetVersion returns resource version
 func (r *Resource) GetVersion() string {
 	return r.version
 }
 
+// GetFetchedAt returns resource fetched at
 func (r *Resource) GetFetchedAt() time.Time {
 	return r.fetchedAt
 }
 
+// GetMetaData returns resource metadata
 func (r *Resource) GetMetaData() []MetaData {
 	return r.metaData
 }
 
+// GetKind returns resource kind
 func (r *Resource) GetKind() string {
 	return r.kind
 }
 
+// GetExternalID returns resource external ID
 func (r *Resource) GetExternalID() string {
 	return r.externalID
 }
 
+// GetUUID returns resource UUID
 func (r *Resource) GetUUID() string {
 	return r.uuid
 }
 
+// GetName returns resource name
 func (r *Resource) GetName() string {
 	return r.name
 }
 
+// GetRelatedResources returns related resources
 func (r *Resource) GetRelatedResources() []Resource {
 	return r.relatedWith
 }
@@ -126,6 +139,7 @@ func (r *Resource) AddRelation(relatedResource Resource) {
 	r.relatedWith = append(r.relatedWith, relatedResource)
 }
 
+// AddMetaData adds or updates metadata for a resource
 func (r *Resource) AddMetaData(metaDataMap map[string]string) {
 	for k, v := range metaDataMap {
 		r.metaData = append(r.metaData, MetaData{
@@ -145,6 +159,7 @@ func (r *Resource) AddMetaDataMultiple(metaMap map[string]string) {
 	}
 }
 
+// FindMetaValue finds the value of a metadata key
 func (r *Resource) FindMetaValue(key string) string {
 	for _, v := range r.metaData {
 		if v.Key == key {
