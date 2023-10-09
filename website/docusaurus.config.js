e72e6d29ae03e1a6d22b302cc1d4da85399084a3
Shaharia Azam <mail@shaharia.com>
2023-10-09 12:33:49 +0200

    Fix deployment

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -18,8 +18,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'shaharia-lab', // Usually your GitHub org/user name.
+  projectName: 'teredix', // Usually your repo name.
+  deploymentBranch: "gh-pages",
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
