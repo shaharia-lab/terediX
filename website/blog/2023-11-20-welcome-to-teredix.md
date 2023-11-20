101660507f265c8d88744ccaaac0633a6eeb5aa4
Shaharia Azam <mail@shaharia.com>
2023-11-20 18:15:19 +0100

    Upgraded docusaurus (#82)
    * Upgraded docusaurus
    
    * Removed un-necessary contents

---
+++ website/blog/2023-11-20-welcome-to-teredix.md
@@ -0,0 +1,12 @@
+---
+slug: welcome-to-teredix
+title: Welcome to TerediX
+authors:
+  name: Shaharia Azam
+  title: Shaharia Lab
+  url: https://github.com/shahariaazam
+  image_url: https://github.com/shahariaazam.png
+tags: [teredix]
+---
+
+Welcome to TerediX, a tech resource discovery project.
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require('prism-react-renderer').themes.github;
+const darkCodeTheme = require('prism-react-renderer').themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -79,6 +79,11 @@ const config = {
           {
             to: 'https://github.com/shaharia-lab/terediX/releases',
             label: 'Releases',
+            position: 'left'
+          },
+          {
+            to: '/blog',
+            label: 'Blog',
             position: 'left'
           },
           {
